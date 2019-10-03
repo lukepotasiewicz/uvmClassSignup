cbfb338023e5d7e4190cfa5c191d7b7fcd406087
Luke Potasiewicz <lpotasiewicz@tesla.com>
2019-10-03 11:35:49 -0400

    Added siple json file database

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.status(200).json({healthy: true}));
+app.get('/read', (req, res) => {
+  const contents = fs.readFileSync('database.json');
+  const jsonData = JSON.parse(contents);
+  res.status(200).json({data: jsonData});
+});
+
+app.get('/write', (req, res) => {
+  const data = JSON.stringify(req.query);
+  fs.writeFileSync('database.json', data);
+  res.status(200).json({querySring: req.query});
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
