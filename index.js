f28c29ce9e71d6a9b91e8cc5ffc5b1ac0aad6344
Luke <lukepotasiewicz@gmail.com>
2019-10-03 11:42:54 -0400

    created endpoints without databse

--- index.js
+++ index.js
@@ -2,6 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.status(200).json({healthy: true}));
+app.get('/', (req, res) => res.send("welcome to my proxy server"));
+app.get('/health', (req, res) => res.status(200).json({healthy: true}));
+
+app.get('/getClasses', (req, res) => {
+    res.status(200).json({
+        user: req.query.user,
+        classes: [
+            {crn: 123, name: "Yeet Class"},
+            {crn: 234, name: "Yote Class"},
+            {crn: 345, name: "Yurt Class"},
+            {crn: 456, name: "Turtle Class"},
+        ]
+    })
+});
+
+app.post('/addClass', (req, res) => {
+    res.status(200).json({...req.query})
+});
+
+app.post('/createUser', (req, res) => {
+    res.status(200).json({...req.query})
+});
+
+app.delete('/deleteClass', (req, res) => {
+    res.status(200).json({...req.query})
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
