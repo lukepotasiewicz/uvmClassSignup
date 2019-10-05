dd4056fa724c567234307b11887ab2daf01b284c
Luke <lukepotasiewicz@gmail.com>
2019-10-05 12:42:49 -0400

    Added database and updated endpoints to work with db

--- index.js
+++ index.js
@@ -3,42 +3,53 @@ const fs = require('fs');
 const app = express();
 const port = 3000;
 
+const readDatabase = () => JSON.parse(fs.readFileSync('database.json'));
+
+const getUser = (netId) => {
+    const jsonData = readDatabase();
+    const userData = jsonData[netId];
+};
+
+const writeDatabase = (data) => {
+    const oldData = readDatabase();
+    fs.writeFileSync('database.json',  JSON.stringify({...oldData, ...data}));
+};
+
 app.get('/', (req, res) => res.send("welcome to my proxy server"));
+
 app.get('/health', (req, res) => res.status(200).json({healthy: true}));
 
+// ----------- GET -----------
 app.get('/getClasses', (req, res) => {
-    res.status(200).json({
-        user: req.query.user,
-        classes: [
-            {crn: 123, name: "Yeet Class"},
-            {crn: 234, name: "Yote Class"},
-            {crn: 345, name: "Yurt Class"},
-            {crn: 456, name: "Turtle Class"},
-        ]
-    })
+    if (!req.query.netId) return res.status(400).json({error: "Bad request"});
+    const userData = getUser(req.query.netId);
+    if (!userData) return res.status(500).json({error: "User not found"});
+    return res.status(userData.error ? 500 : 200).json(userData);
 });
 
-app.post('/addClass', (req, res) => {
-    res.status(200).json({...req.query})
+// ----------- ADD -----------
+app.get('/addUser', (req, res) => {
+    if (!req.query.netId) return res.status(400).json({error: "Bad request"});
+    const userData = getUser(req.query.netId);
+    if (!userData) return res.status(500).json({error: "User not found"});
+    const newUser = { netId: req.query.netId, classes: [] };
+    writeDatabase({[req.query.netId]: newUser});
+    return res.status(200).json({queryString: req.query});
 });
 
-app.post('/createUser', (req, res) => {
-    res.status(200).json({...req.query})
+app.get('/addClasses', (req, res) => {
+    if (!req.query.netId || !req.query.classes) return res.status(400).json({error: "Bad request"});
+    const userData = getUser(req.query.netId);
+    if (!userData) return res.status(500).json({error: "User not found"});
+    userData.classes = userData.classes.concat(req.query.classes.split(','));
+    writeDatabase({[req.query.netId]: userData});
+    return res.status(200).json({queryString: req.query});
 });
 
-app.delete('/deleteClass', (req, res) => {
-    res.status(200).json({...req.query})
-});
-app.get('/read', (req, res) => {
-  const contents = fs.readFileSync('database.json');
-  const jsonData = JSON.parse(contents);
-  res.status(200).json({data: jsonData});
-});
-
-app.get('/write', (req, res) => {
-  const data = JSON.stringify(req.query);
-  fs.writeFileSync('database.json', data);
-  res.status(200).json({querySring: req.query});
+// ----------- DELETE -----------
+app.get('/deleteClass', (req, res) => {
+    if (!req.query.netId) return res.status(400).json({error: "Bad request"});
+    return res.status(200).json({...req.query});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
